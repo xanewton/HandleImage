4df927bcdf220d4c57e58f15c5092baaa1e61ed7
Angel Newton <xanewton@gmail.com>
2017-04-15 22:41:20 -0500

    Add share image option

--- app/src/main/java/com/xengar/android/handleimage/ui/MainActivity.java
+++ app/src/main/java/com/xengar/android/handleimage/ui/MainActivity.java
@@ -15,7 +15,14 @@
  */
 package com.xengar.android.handleimage.ui;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -23,8 +30,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.xengar.android.handleimage.BuildConfig;
 import com.xengar.android.handleimage.R;
 import com.xengar.android.handleimage.utils.ActivityUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * MainActivity
@@ -104,6 +117,69 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.action_drop_outside:
                 launchDropOutside();
+                return true;
+
+            case R.id.action_share:
+                Context context = getApplicationContext();
+                // Save file to storage
+                Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.gallery_lion);
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                icon.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                File imagePath2 = new File(context.getFilesDir(), "internal");
+                File f = new File(imagePath2, "gallery_lion.jpg");
+                context.deleteFile(f.getName());
+                //if(f.exists()){
+                //    Log.i("imageExistsInternal", "file is complete");
+                //} else {
+                FileOutputStream outputStream = null;
+                try {
+                    outputStream = new FileOutputStream(f, false);
+                    outputStream.write(bytes.toByteArray());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (outputStream != null) {
+                        try {
+                            outputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                //}
+
+                //ActivityUtils.launchShareImage(this, createShareText());
+                // https://medium.com/google-developers/sharing-content-between-android-apps-2e6db9d1368b#.6usvw9n9p
+                // https://developer.android.com/reference/android/support/v4/content/FileProvider.html
+
+                // Directory must be in file_paths.xml
+                File imagePath = new File(context.getFilesDir(), "internal");
+                File imageFile = new File(imagePath, "gallery_lion.jpg");
+                Uri uriToImage = FileProvider.getUriForFile(context, BuildConfig.FILES_AUTHORITY, imageFile);
+                // content://${files_authority}/private/file.jpg
+                Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                        .setStream(uriToImage)
+                        .setType("image/*")
+                        .getIntent();
+                // Provide read access
+                shareIntent.setData(uriToImage);
+                shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                if (shareIntent.resolveActivity(getPackageManager()) != null) {
+                    //startActivity(shareIntent);
+                    //startActivity(Intent.createChooser(shareIntent, "Share Image"));
+
+                    Intent shareIntent2 = new Intent();
+                    shareIntent2.setAction(Intent.ACTION_SEND);
+                    shareIntent2.putExtra(Intent.EXTRA_TEXT, "gallery_lion.jpg");
+                    shareIntent2.putExtra(Intent.EXTRA_STREAM, uriToImage);
+                    shareIntent2.setType("image/*");
+                    //shareIntent2.setData(uriToImage);
+                    shareIntent2.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(Intent.createChooser(shareIntent2, "Share images..."));
+                }
+
+                //ActivityUtils.firebaseAnalyticsLogEventSelectContent(mFirebaseAnalytics,
+                //        "Animal: " + animal.getName() + ", AnimalId: " + animalID, TYPE_SHARE, TYPE_SHARE);
                 return true;
         }
 
